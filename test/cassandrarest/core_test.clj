dc94bb20080aa14d8e1801d0160473a1e3acb8c3
Joey Gibson <jgibson@lancope.com>
2016-03-02 18:10:05 -0700

    initial commit

---
+++ test/cassandrarest/core_test.clj
@@ -0,0 +1,7 @@
+(ns cassandrarest.core-test
+  (:require [clojure.test :refer :all]
+            [cassandrarest.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
